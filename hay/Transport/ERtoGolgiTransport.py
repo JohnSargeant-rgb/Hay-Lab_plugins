e9569ba5b9fb6c99427c50715ff4dfb0dafcfabc
JohnSargeant-rgb <63664582+JohnSargeant-rgb@users.noreply.github.com>
2021-01-20 22:56:46 -0700

    Update ERtoGolgiTransport.py

--- hay/Transport/ERtoGolgiTransport.py
+++ hay/Transport/ERtoGolgiTransport.py
@@ -190,7 +190,7 @@ gdp.addMessage("    ", italicFont)
 progress = images_processed(dest)
 gdp.addMessage(str(progress[0])+'% Images Assayed' + ' ' +
                '(' + str(progress[2]) + '/' + str(progress[1]) + ')', italicFont)
-gdp.addHelp("https://github.com/JohnSargeant-rbg?tab=projects")
+gdp.addHelp("https://github.com/JohnSargeant-rgb?tab=projects")
 gdp.showDialog()
 if gdp.wasOKed():
     dest = gdp.getNextString().strip()
